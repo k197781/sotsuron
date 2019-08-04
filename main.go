1dc4a81cade2fcc8bab8f1e582e76cf62b379141
ItohKaori <k197781@kansai-u.ac.jp>
2019-08-04 14:04:38 +0900

    set environment variable

--- main.go
+++ main.go
@@ -1,19 +1,30 @@
 package main
 
-import(
+import (
 	"fmt"
-	"os/exec"
-	"time"
 	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
+	maxConnection := 1000
 	for {
-		out, err := exec.Command("sh", "-c", "netstat -tan | grep ':80' | wc -l").Output()
+		out, err := exec.Command("sh", "-c", "netstat -tan | grep '3000' | wc -l").Output()
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(string(out))
+		connectionStr := strings.Replace(string(out), " ", "", -1)
+		connectionStr = strings.Replace(connectionStr, "\n", "", -1)
+		connection, _ := strconv.Atoi(connectionStr)
+		fmt.Println(connection)
+		if connection > maxConnection {
+			os.Setenv("ISATTACKED", "false")
+			fmt.Println(os.Getenv("ISATTACKED"))
+		}
 		time.Sleep(1 * time.Second)
 	}
 }
